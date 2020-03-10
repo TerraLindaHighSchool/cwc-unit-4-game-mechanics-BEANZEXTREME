639a68aaf84605d106f37f8fa3fd97c31e159c77
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2020-03-09 21:17:05 -0700

    4.4.2
    done

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-          Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave();
     }
 
     // Update is called once per frame
@@ -27,5 +27,13 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;    
+    }
+
+    void SpawnEnemyWave()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
